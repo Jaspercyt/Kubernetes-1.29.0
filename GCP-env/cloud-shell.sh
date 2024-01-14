f6f58161dbd8db47dcd249dfa06b989406cc23b0
Jaspercyt <88648972+Jaspercyt@users.noreply.github.com>
2024-01-14 21:36:54 +0800

    Add files via upload

---
+++ GCP-env/cloud-shell.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+# 定義網絡和虛擬機器的配置變數
+NETWORK="gcp-kubernetes-vpc"  # VPC 名稱
+SUBNET="gcp-kubernetes-subnet"  # 子網絡名稱
+REGION="us-west4"  # 使用的地區
+SUBNET_RANGE="192.168.56.0/24"  # 子網絡 IP 範圍
+MACHINE_TYPE="e2-medium"  # 虛擬機器類型
+IMAGE_FAMILY="ubuntu-2204-lts"  # 虛擬機器使用的映像檔案族
+IMAGE_PROJECT="ubuntu-os-cloud"  # 映像檔案所屬的專案
+BOOT_DISK_SIZE="10GB"  # 啟動盤大小
+BOOT_DISK_TYPE="pd-standard"  # 啟動盤類型
+
+# 建立自定義模式的 VPC 網絡
+gcloud compute networks create $NETWORK --subnet-mode=custom
+# 在剛建立的 VPC 網絡中創建子網絡
+gcloud compute networks subnets create $SUBNET --network=$NETWORK --region=$REGION --range=$SUBNET_RANGE
+
+# 定義一系列的防火牆規則
+FIREWALL_RULES=(
+  "gcp-kubernetes-vpc-allow-icmp icmp INGRESS 65534 0.0.0.0/0"
+  "gcp-kubernetes-vpc-allow-ssh tcp:22 INGRESS 65534 0.0.0.0/0"
+  "allow-http tcp:80 INGRESS http-server"
+  "allow-https tcp:443 INGRESS https-server"
+  "allow-lb-health-check tcp:8080 INGRESS 35.191.0.0/16,130.211.0.0/22"
+  "gcp-kubernetes-vpc-allow-internal icmp,tcp,udp INGRESS 1000 192.168.56.0/24 192.168.56.0/24"
+)
+
+# 循環遍歷防火牆規則陣列，為每一條規則建立防火牆
+for rule in "${FIREWALL_RULES[@]}"; do
+  read -r name allow direction priority source_ranges destination_ranges <<<"$rule"
+  gcloud compute firewall-rules create $name --network=$NETWORK --allow=$allow --direction=$direction --priority=$priority --source-ranges=${source_ranges:-0.0.0.0/0} ${destination_ranges:+--destination-ranges=$destination_ranges}
+done
+
+# 定義虛擬機器的名稱和私有網絡 IP
+INSTANCE_NAMES=("master 192.168.56.10" "worker01 192.168.56.11" "worker02 192.168.56.12")
+
+# 為每一台虛擬機器建立實例
+for instance in "${INSTANCE_NAMES[@]}"; do
+  read -r name ip <<<"$instance"
+  gcloud compute instances create $name \
+    --zone=${REGION}-a \
+    --machine-type=$MACHINE_TYPE \
+    --network=$NETWORK \
+    --subnet=$SUBNET \
+    --network-tier=STANDARD \
+    --maintenance-policy=MIGRATE \
+    --scopes=default \
+    --tags=http-server,https-server \
+    --image-family=$IMAGE_FAMILY \
+    --image-project=$IMAGE_PROJECT \
+    --boot-disk-size=$BOOT_DISK_SIZE \
+    --boot-disk-type=$BOOT_DISK_TYPE \
+    --boot-disk-device-name=$name \
+    --private-network-ip=$ip
+done
---
+++ GCP-env/worker-node.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+ZONE="us-west4-a"
+SETUP_SCRIPT_URL="https://raw.githubusercontent.com/Jaspercyt/Kubernetes-1.29.0/main/GCP-env/GCE-shell/1-setup-node.sh"
+NODE_IPS=("192.168.56.11" "192.168.56.12")
+
+setup_node() {
+  local node=$1
+  local ip=$2
+
+  gcloud compute ssh $node --zone=$ZONE --command="wget -O /home/\$(whoami)/1-setup-node.sh $SETUP_SCRIPT_URL && bash /home/\$(whoami)/1-setup-node.sh"
+
+  gcloud compute scp kubeadm-join $node:/home/\$(whoami)/ --zone=$ZONE
+
+  gcloud compute ssh $node --zone=$ZONE --command="chmod +x /home/\$(whoami)/kubeadm-join && sudo sh /home/\$(whoami)/kubeadm-join"
+
+  gcloud compute ssh $node --zone=$ZONE --command="sudo sed -i 'a KUBELET_EXTRA_ARGS=\"--node-ip=$ip\"' /var/lib/kubelet/kubeadm-flags.env && sudo systemctl daemon-reload && sudo systemctl restart kubelet"
+
+  gcloud compute ssh $node --zone=$ZONE --command="rm -rf /home/\$(whoami)/1-setup-node.sh /home/\$(whoami)/cni-plugins-linux-amd64-v1.4.0.tgz /home/\$(whoami)/containerd-1.7.11-linux-amd64.tar.gz /home/\$(whoami)/kubeadm-join /home/\$(whoami)/runc.amd64"
+}
+
+for i in ${!NODE_IPS[@]}; do
+  setup_node "worker0$((i+1))" "${NODE_IPS[$i]}"
+done
