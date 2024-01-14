4b9ade341b8d337040e3bfccae1eec3eb446223b
Jaspercyt <88648972+Jaspercyt@users.noreply.github.com>
2024-01-14 21:44:33 +0800

    Add files via upload

--- GCP-env/GCE-Kubernetes.sh
+++ GCP-env/GCE-Kubernetes.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-wget https://raw.githubusercontent.com/Jaspercyt/Kubernetes-1.29.0/main/GCP-env/cloud-shell.sh
-wget https://raw.githubusercontent.com/Jaspercyt/Kubernetes-1.29.0/main/GCP-env/master-node.sh
-wget https://raw.githubusercontent.com/Jaspercyt/Kubernetes-1.29.0/main/GCP-env/worker-node.sh
-bash cloud-shell.sh
-bash master-node.sh
-bash worker-node.sh
+wget https://raw.githubusercontent.com/Jaspercyt/Kubernetes-1.29.0/main/GCP-env/cloud-shell.sh && bash cloud-shell.sh
+wget https://raw.githubusercontent.com/Jaspercyt/Kubernetes-1.29.0/main/GCP-env/master-node.sh && bash master-node.sh
+wget https://raw.githubusercontent.com/Jaspercyt/Kubernetes-1.29.0/main/GCP-env/worker-node.sh && bash worker-node.sh
