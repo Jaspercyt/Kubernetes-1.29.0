f6f7ce94a788566f7423699f71ee7f63af728bbd
Jaspercyt <88648972+Jaspercyt@users.noreply.github.com>
2024-01-17 22:20:53 +0800

    Update delete-k8s-gcp-resources.sh

--- GCP-env/delete-k8s-gcp-resources.sh
+++ GCP-env/delete-k8s-gcp-resources.sh
@@ -1,25 +1,39 @@
 #!/bin/bash
 
-# 設定 GCP 資源名稱
-NETWORK="gcp-kubernetes-vpc"
-SUBNET="gcp-kubernetes-subnet"
-FIREWALL_RULES=("gcp-kubernetes-vpc-allow-icmp" "gcp-kubernetes-vpc-allow-ssh" "allow-http" "allow-https" "allow-lb-health-check" "gcp-kubernetes-vpc-allow-internal")
+# 定義區域
+ZONE="us-west4-a"
+
+# 刪除所有VM實例
 INSTANCE_NAMES=("master" "worker01" "worker02")
 
-# 刪除 VM 實例
-for instance in "${INSTANCE_NAMES[@]}"; do
-  gcloud compute instances delete $instance --quiet --zone=us-west4-a
+for name in "${INSTANCE_NAMES[@]}"; do
+  gcloud compute instances delete $name --zone=$ZONE --quiet
 done
 
+# 刪除VPC網路和子網絡
+NETWORK="gcp-kubernetes-vpc"
+SUBNET="gcp-kubernetes-subnet"
+
+gcloud compute networks subnets delete $SUBNET --region=$REGION --quiet
+gcloud compute networks delete $NETWORK --quiet
+
 # 刪除防火牆規則
+FIREWALL_RULES=(
+  "gcp-kubernetes-vpc-allow-icmp"
+  "gcp-kubernetes-vpc-allow-ssh"
+  "allow-http"
+  "allow-https"
+  "allow-lb-health-check"
+  "gcp-kubernetes-vpc-allow-internal"
+)
+
 for rule in "${FIREWALL_RULES[@]}"; do
   gcloud compute firewall-rules delete $rule --quiet
 done
 
-# 刪除子網絡
-gcloud compute networks subnets delete $SUBNET --quiet
+# 刪除其他資源
+KUBEADM_DIR="/home/$(whoami)/kubeadm-token"
+rm -rf $KUBEADM_DIR
 
-# 刪除網絡
-gcloud compute networks delete $NETWORK --quiet
-
-echo "所有資源已被刪除。"
+# 清理完畢
+echo "所有GCP資源已成功刪除。"
