63ee5ccc65cc189d89ae735cf446d9b094f15328
Jaspercyt <88648972+Jaspercyt@users.noreply.github.com>
2024-01-17 23:44:46 +0800

    Update delete-k8s-gcp-resources.sh

--- GCP-env/delete-k8s-gcp-resources.sh
+++ GCP-env/delete-k8s-gcp-resources.sh
@@ -1,32 +1,13 @@
 #!/bin/bash
 
-# 定義區域
-ZONE="us-west4-a"
+# 刪除 VM Instance
+gcloud compute instances delete master worker01 worker02 --zone=us-west4-a --quiet
 
-# 刪除所有VM實例
-INSTANCE_NAMES=("master" "worker01" "worker02")
+# 刪除 Firewallrules
+gcloud compute firewall-rules delete allow-https gcp-kubernetes-vpc-allow-icmp gcp-kubernetes-vpc-allow-ssh allow-http gcp-kubernetes-vpc-allow-internal allow-lb-health-check --quiet
 
-for name in "${INSTANCE_NAMES[@]}"; do
-  gcloud compute instances delete $name --zone=$ZONE --quiet
-done
+# 刪除 Subnet
+gcloud compute networks subnets delete gcp-kubernetes-subnet --region us-west4 --quiet
 
-# 刪除VPC網路和子網絡
-NETWORK="gcp-kubernetes-vpc"
-SUBNET="gcp-kubernetes-subnet"
-
-gcloud compute networks subnets delete $SUBNET --region=$REGION --quiet
-gcloud compute networks delete $NETWORK --quiet
-
-# 刪除防火牆規則
-gcloud compute firewall-rules delete gcp-kubernetes-vpc-allow-icmp
-gcloud compute firewall-rules delete gcp-kubernetes-vpc-allow-ssh
-gcloud compute firewall-rules delete allow-http
-gcloud compute firewall-rules delete allow-https
-gcloud compute firewall-rules delete allow-lb-health-check
-gcloud compute firewall-rules delete gcp-kubernetes-vpc-allow-internal
-
-# 刪除子網
-gcloud compute networks subnets delete gcp-kubernetes-subnet --region=us-west4
-
-# 刪除網路
-gcloud compute networks delete gcp-kubernetes-vpc
+# 刪除 VPC
+gcloud compute networks delete gcp-kubernetes-vpc --quiet
