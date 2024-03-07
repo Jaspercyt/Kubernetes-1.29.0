16835fc7f7156af37e2a6960a1343e6e78dae0bc
Jaspercyt <88648972+Jaspercyt@users.noreply.github.com>
2024-03-07 15:55:05 +0800

    Add files via upload

--- GCP-env/delete-k8s-gcp-resources.sh
+++ GCP-env/delete-k8s-gcp-resources.sh
@@ -3,7 +3,7 @@
 # 刪除 VM Instance
 gcloud compute instances delete master worker01 worker02 --zone=us-west4-a --quiet
 
-# 刪除 Firewallrules
+# 刪除 Firewall rules
 gcloud compute firewall-rules delete allow-https gcp-kubernetes-vpc-allow-icmp gcp-kubernetes-vpc-allow-ssh allow-http gcp-kubernetes-vpc-allow-internal allow-lb-health-check --quiet
 
 # 刪除 Subnet
--- GCP-env/worker-node.sh
+++ GCP-env/worker-node.sh
@@ -1,32 +1,29 @@
 #!/bin/bash
 
-# 定義 Google Cloud 的區域
-ZONE="us-west4-a"
+# 定義環境變數
+ZONE="us-west4-a"                               # 指定 Region
+SETUP_SCRIPT_URL="https://raw.githubusercontent.com/Jaspercyt/Kubernetes-1.29.0/main/GCP-env/GCE-shell/1-setup-node.sh" # 設定節點的腳本 URL
+NODE_IPS=("192.168.56.11" "192.168.56.12")      # worker node 的 IP 陣列
 
-# 定義節點設置腳本的 URL
-SETUP_SCRIPT_URL="https://raw.githubusercontent.com/Jaspercyt/Kubernetes-1.29.0/main/GCP-env/GCE-shell/1-setup-node.sh"
-
-# 定義節點的 IP 位址
-NODE_IPS=("192.168.56.11" "192.168.56.12")
-
-# 定義一個函數來設置節點
+# 定義 setup_node 函式
 setup_node() {
-  local node=$1  # 節點名稱
-  local ip=$2    # 節點 IP
+  local node=$1  # 參數1: 節點名稱
+  local ip=$2    # 參數2: 節點 IP 地址
 
-  # 通過 SSH 連接到節點，並下載並執行節點設置腳本
+  # 透過 gcloud compute ssh 命令遠端登入節點：
+  # 1. 下載設定節點所需的腳本到用戶家目錄並執行
   gcloud compute ssh $node --zone=$ZONE --command="wget -O /home/\$(whoami)/1-setup-node.sh $SETUP_SCRIPT_URL && bash /home/\$(whoami)/1-setup-node.sh"
-  # 將 kubeadm-join 腳本複製到節點
+  # 2. 把 kubeadm-join 腳本複製到節點上，讓 worker node 加入 Kubernetes 叢集
   gcloud compute scp kubeadm-join $node:'/home/$(whoami)/' --zone=$ZONE
-  # 在節點上設置 kubeadm-join 腳本的執行權限，並執行它
+  # 3. 設定 kubeadm-join 腳本執行權限並加入叢集
   gcloud compute ssh $node --zone=$ZONE --command="chmod +x /home/\$(whoami)/kubeadm-join && sudo sh /home/\$(whoami)/kubeadm-join"
-  # 更新節點的 kubelet 配置，並重啟服務
+  # 4. 修改 kubelet 的啟動參數，設定節點 IP 並重啟 kubelet
   gcloud compute ssh $node --zone=$ZONE --command="sudo sed -i 'a KUBELET_EXTRA_ARGS=\"--node-ip=$ip\"' /var/lib/kubelet/kubeadm-flags.env && sudo systemctl daemon-reload && sudo systemctl restart kubelet"
-  # 清理節點上的臨時檔案
+  # 5. 清理安裝過程中產生的臨時檔案
   gcloud compute ssh $node --zone=$ZONE --command="rm -rf /home/\$(whoami)/1-setup-node.sh /home/\$(whoami)/cni-plugins-linux-amd64-v1.4.0.tgz /home/$(whoami)/containerd-1.7.11-linux-amd64.tar.gz /home/$(whoami)/kubeadm-join /home/$(whoami)/runc.amd64"
 }
 
-# 遍歷每個節點 IP 位址，並調用 setup_node 函數來設置對應的節點
+# 迴圈讀取 NODE_IPS 陣列，對每個 worker node 調用 setup_node 函式進行設定
 for i in ${!NODE_IPS[@]}; do
   setup_node "worker0$((i+1))" "${NODE_IPS[$i]}"
 done
